6ac375e6db3fe7d0ce42fdac478c34685b8f14d0
Alvin Kamau <alvin.kamau@student.moringaschool.com>
2024-07-02 13:03:54 +0300

    Assignments

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride<=400){
+    return "This one is on me!"
+  }
+  else if(ride<=2000){
+    return"That will be twenty bucks."
+  }
+  else if(ride<=2500){
+   return"I will gladly take your thirty bucks."
+  }
+  else{
+    return "No can do."
+  }
+}
+function ternaryCheckCity(city){
+    // Write your code here!
+    if (city==='NYC'){
+      return "Ok, sounds good."
+    }
+    else{
+      return "No go."
+    }
+
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(answer){
   // Write your code here!
+if(answer==="generous"){
+  return "Thank you so much."
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+else if (answer==="not as generous"){
+  return  "Thank you."
+}
+else{
+  return "Bye."
+}
 }
